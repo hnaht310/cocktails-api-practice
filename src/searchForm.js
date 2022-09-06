3eb026426ca091883ffa57b6c5974e9006cb7bbd
TP <thanhpn1@yahoo.com>
2022-09-05 20:19:29 -0500

    fixed search form

--- src/searchForm.js
+++ src/searchForm.js
@@ -8,12 +8,11 @@ const input = getElement('[name="drink"]');
 input.addEventListener('keyup', async (e) => {
   console.log(e.target.value);
   const value = e.target.value;
-  if (value) {
-    const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${value}`;
-    const drinksData = await fetchDrinks(url);
-    // console.log(drinksData);
-    displayDrinks(drinksData);
-  }
+  if (!value) return;
+  const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${value}`;
+  const drinksData = await fetchDrinks(url);
+  // console.log(drinksData);
+  displayDrinks(drinksData);
 });
 
 form.addEventListener('submit', (e) => {
